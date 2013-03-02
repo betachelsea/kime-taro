7837a8f220f2de9a316f8ba3510078a7d86dce1e
betachelsea <beta.chelsea@gmail.com>
2013-03-02 15:32:13 +0900

    first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KimeTaro::Application.config.secret_token = 'f820baad4858a1e5a30801ac7016f7d6ac7dbb84c88dcfae3172dbda47674243bbe8bbe3d143aa3d785658019bb35952b86fafd06845bf90c0ce4f0f68197544'
